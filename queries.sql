566ae8eb466fcebb052c8aae5b09609bca524c6c
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-06-25 13:20:10 -0700

    finished raw SQL queries

--- queries.sql
+++ queries.sql
@@ -17,4 +17,21 @@ WHERE "OrderDate" < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT o."OrderId",
+    p."ProductName",
+    o."Quantity" AS "ProductQuantity"
+FROM "OrderDetail" AS o
+    JOIN "Product" AS p
+    ON o."ProductId" = p."Id"
+WHERE o."OrderId" = 10251
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o."Id" AS "OrderId",
+    c."CompanyName" AS "CustomerCompanyName",
+    e."LastName" AS "EmployeeLastName"
+FROM "Order" AS o
+    JOIN "Customer" AS c
+    JOIN "Employee" AS e  
+WHERE o."CustomerId" = c."Id"
+    AND o."EmployeeId" = e."Id"
